bb72e23f44252e44f0327de4f41d01987a2e5da8
André-Patrick Bubel <code@andre-bubel.de>
2018-12-25 13:11:22 +0100

    slic3r-prusa3d: Dependency cleanup, perl isn't needed anymore

--- pkgs/slic3r-prusa3d/default.nix
+++ pkgs/slic3r-prusa3d/default.nix
@@ -1,35 +1,8 @@
-{ stdenv, lib, fetchFromGitHub, makeWrapper, which, cmake, perl, perlPackages,
-  boost, tbb, wxGTK30, pkgconfig, gtk3, fetchurl, gtk2, libGLU,
-  glew, eigen, curl, gtest, nlopt, pcre, xorg }:
+{ stdenv, lib, fetchFromGitHub, makeWrapper, cmake, pkgconfig
+, boost, curl, expat, glew, libpng, tbb, wxGTK30
+, gtest, nlopt
+}:
 let
-  AlienWxWidgets = perlPackages.buildPerlPackage rec {
-    name = "Alien-wxWidgets-0.69";
-    src = fetchurl {
-      url = "mirror://cpan/authors/id/M/MD/MDOOTSON/${name}.tar.gz";
-      sha256 = "075m880klf66pbcfk0la2nl60vd37jljizqndrklh5y4zvzdy1nr";
-    };
-    propagatedBuildInputs = [
-      pkgconfig perlPackages.ModulePluggable perlPackages.ModuleBuild
-      gtk2 gtk3 wxGTK30
-    ];
-  };
-
-  Wx = perlPackages.Wx.overrideAttrs (oldAttrs: {
-    propagatedBuildInputs = [
-      perlPackages.ExtUtilsXSpp
-      AlienWxWidgets
-    ];
-  });
-
-  WxGLCanvas = perlPackages.buildPerlPackage rec {
-    name = "Wx-GLCanvas-0.09";
-    src = fetchurl {
-      url = "mirror://cpan/authors/id/M/MB/MBARBON/${name}.tar.gz";
-      sha256 = "1q4gvj4gdx4l8k4mkgiix24p9mdfy1miv7abidf0my3gy2gw5lka";
-    };
-    propagatedBuildInputs = [ Wx perlPackages.OpenGL libGLU ];
-    doCheck = false;
-  };
   nloptVersion = if lib.hasAttr "version" nlopt
                  then lib.getAttr "version" nlopt
                  else "2.4";
@@ -43,43 +16,20 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [
     cmake
     makeWrapper
+    pkgconfig
   ];
 
+  # We could add Eigen, but it doesn't currently compile with the version in
+  # nixpkgs.
   buildInputs = [
-    curl
-    #eigen
-    glew
-    pcre
-    perl
-    tbb
-    which
-    Wx
-    WxGLCanvas
-    xorg.libXdmcp
-    xorg.libpthreadstubs
-  ] ++ checkInputs ++ (with perlPackages; [
     boost
-    ClassXSAccessor
-    EncodeLocale
-    ExtUtilsMakeMaker
-    ExtUtilsTypemapsDefault
-    ExtUtilsXSpp
-    GrowlGNTP
-    ImportInto
-    IOStringy
-    locallib
-    LWP
-    MathClipper
-    MathConvexHullMonotoneChain
-    MathGeometryVoronoi
-    MathPlanePath
-    ModuleBuildWithXSpp
-    Moo
-    NetDBus
-    OpenGL
-    threads
-    XMLSAX
-  ]);
+    curl
+    expat
+    glew
+    libpng
+    tbb
+    wxGTK30
+  ] ++ checkInputs;
 
   checkInputs = [ gtest ];
 
@@ -94,13 +44,6 @@ stdenv.mkDerivation rec {
     # on other distributions. As the copy in glibc seems to be identical to the
     # one in the kernel, we use that one instead.
     sed -i 's|"/usr/include/asm-generic/ioctls.h"|<asm-generic/ioctls.h>|g' src/libslic3r/GCodeSender.cpp
-
-    # PERL_VENDORARCH and PERL_VENDORLIB aren't set correctly by the build
-    # system, so we have to override them. Setting them as environment variables
-    # doesn't work though, so substituting the paths directly in CMakeLists.txt
-    # seems to be the easiest way.
-    sed -i "s|\''${PERL_VENDORARCH}|$out/lib/slic3r-prusa3d|g" xs/CMakeLists.txt
-    sed -i "s|\''${PERL_VENDORLIB}|$out/lib/slic3r-prusa3d|g" xs/CMakeLists.txt
   '' + lib.optionalString (lib.versionOlder "2.5" nloptVersion) ''
     # Since version 2.5.0 of nlopt we need to link to libnlopt, as libnlopt_cxx
     # now seems to be integrated into the main lib.
@@ -110,10 +53,6 @@ stdenv.mkDerivation rec {
   postInstall = ''
     mkdir -p $out/bin
     cp src/slic3r-gui $out/bin
-
-    wrapProgram "$out/bin/slic3r-gui" \
-    --prefix PERL5LIB : "$out/lib/slic3r-prusa3d:$PERL5LIB"
-
     cp -r ../resources $out
   '';
 
