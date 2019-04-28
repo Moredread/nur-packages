fe0af1c8eea0b1462491c5f1b6f20acadfcba9f8
André-Patrick Bubel <code@andre-bubel.de>
2019-04-28 19:56:42 +0200

    latte: Remove as I don't use it

--- default.nix
+++ default.nix
@@ -17,7 +17,6 @@ rec {
   croc = pkgs.callPackage ./pkgs/croc { };
   i3status-rust = pkgs.callPackage ./pkgs/i3status-rust { };
   implicitcad = pkgs.haskellPackages.callPackage ./pkgs/implicitcad { };
-  latte = pkgs.callPackage ./pkgs/latte { };
   nix-search = pkgs.callPackage ./pkgs/nix-search { };
   throttled = pkgs.callPackage ./pkgs/throttled { };
   lenovo-throttling-fix = throttled;
