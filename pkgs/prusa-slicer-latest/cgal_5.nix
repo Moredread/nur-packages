aca404542d822c50aa8747db0238cffaf8b516e6
André-Patrick Bubel <code@apb.name>
2021-06-12 02:06:36 +0200

    stdenv.lib -> lib

--- pkgs/prusa-slicer-latest/cgal_5.nix
+++ pkgs/prusa-slicer-latest/cgal_5.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, cmake, boost, gmp, mpfr }:
+{ stdenv, lib, fetchFromGitHub, cmake, boost, gmp, mpfr }:
 
 stdenv.mkDerivation rec {
   version = "5.0.1";
@@ -18,7 +18,7 @@ stdenv.mkDerivation rec {
 
   doCheck = false;
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "Computational Geometry Algorithms Library";
     homepage = "http://cgal.org";
     license = with licenses; [ gpl3Plus lgpl3Plus];
